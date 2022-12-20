be0c9d33d4de45f588b365acf3b005427b33b68f
Resmi Manoharan Achary Remani <resmi.manoharanacharyremani@technipenergies.com>
2022-12-20 10:20:03 +0400

    async pipe, generics,ngtemplate

--- src/app/modules/dashboard/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/modules/dashboard/pages/user-dashboard/user-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { map, Observable, Subject, take, takeUntil } from 'rxjs';
 import { EmployeeService } from 'src/app/core/services/employee.service';
 import { Employee } from 'src/app/model/employee.model';
 import { User } from 'src/app/model/user.model';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -27,7 +28,24 @@ export class UserDashboardComponent {
   empsList : Employee[] =[];
   private readonly destroying$ = new Subject<boolean>();
 
+  observable$!: Observable<number>;
+
+  /**generics** */
+  myNumArr = this.getArray<number>([100, 200, 300]);
+
+  myStrArr = this.getArray<string>(["Hello", "World"]);
+
+  genericVal = this.displayType<number, string>(1, "Steve");
+
+  hasData:boolean = false;
+  
   ngOnInit(){
+   this.observable$ = this.getObservable();
+
+   //calling gerics function
+   this.myNumArr.push(2);
+
+
 
   this.empService.getList().pipe(takeUntil(this.destroying$)).subscribe({
     next:(data:Employee[])=>{
@@ -37,11 +55,31 @@ export class UserDashboardComponent {
     console.log(error);
    }
   });
+  
+}
+
+getObservable() {
+  return interval(1000).pipe(
+    take(11),
+    map(v => v * 2)
+  );
+}
+
+ getArray<T>(items : T[]) : T[] {
+  return new Array<T>().concat(items);
+}
+
+ displayType<T, U>(id:T, name:U): void { 
+  console.log(typeof(id) + ", " + typeof(name));  
 }
 
   ngOnDestroy(){
     this.destroying$.next(true);
     this.destroying$.complete();
+  }
+
+  getDate(num:number,num2:number):number{
+     return 1;
   }
 
 }
